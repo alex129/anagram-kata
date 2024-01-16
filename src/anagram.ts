9241d7ec0b748a8452e6da1ebc646df851092976
Plk <plikforo@gmail.com>
2024-01-16 21:22:41 +0100

    refactor: :recycle: Refactor code to get all posible anagrams of string

--- src/anagram.ts
+++ src/anagram.ts
@@ -1,16 +1,26 @@
 export class Anagram {
   static handler(str: string) {
-    if (str === "ab") {
-      return "ab ba";
+    const anagrams = this.generate(str);
+    return anagrams.join(" ");
+  }
+
+  static generate(str: string) {
+    if (str.length <= 1) return [str];
+
+    const result: string[] = [];
+    for (let letterIndex = 0; letterIndex < str.length; letterIndex++) {
+      const firstLetter = str[letterIndex];
+      const remainingLetters = `${str.slice(0, letterIndex)}${str.slice(
+        letterIndex + 1
+      )}`;
+
+      const subAnagrams = Anagram.generate(remainingLetters);
+
+      for (const subAnagram of subAnagrams) {
+        result.push(`${firstLetter}${subAnagram}`);
+      }
     }
 
-    if (str === "abc") {
-      return "abc acb bac bca cab cba";
-    }
-
-    if (str === "abcd") {
-      return "abcd abdc acbd acdb adbc adcb bacd badc bcad bcda bdac bdca cabd cadb cbad cbda cdab cdba dabc dacb dbac dbca dcab dcba";
-    }
-    return str;
+    return result;
   }
 }
