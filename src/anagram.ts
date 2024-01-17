4be4b52fb00fabecab9b9cb420c2ced92a707c82
Plk <plikforo@gmail.com>
2024-01-17 08:48:05 +0100

    refactor: :recycle: Refactor to return empty anagram if string is empty

--- src/anagram.ts
+++ src/anagram.ts
@@ -1,26 +1,26 @@
 export class Anagram {
-  static handler(str: string) {
-    const anagrams = this.generate(str);
+  static get(str: string) {
+    const anagrams = Anagram.generate(str);
     return anagrams.join(" ");
   }
 
   static generate(str: string) {
-    if (str.length <= 1) return [str];
+    if (str.length === 1) return [str];
+    if (str.length === 0) return [];
 
-    const result: string[] = [];
+    const anagrams: string[] = [];
     for (let letterIndex = 0; letterIndex < str.length; letterIndex++) {
-      const firstLetter = str[letterIndex];
+      const curentLetter = str[letterIndex];
       const remainingLetters = `${str.slice(0, letterIndex)}${str.slice(
         letterIndex + 1
       )}`;
 
       const subAnagrams = Anagram.generate(remainingLetters);
-
       for (const subAnagram of subAnagrams) {
-        result.push(`${firstLetter}${subAnagram}`);
+        anagrams.push(`${curentLetter}${subAnagram}`);
       }
     }
 
-    return result;
+    return anagrams;
   }
 }
--- src/test/anagram.test.ts
+++ src/test/anagram.test.ts
@@ -2,29 +2,29 @@ import { Anagram } from "../anagram";
 
 describe("Anagram", () => {
   test("should return empty string if the input string is empty", () => {
-    expect(Anagram.handler("")).toBe("");
+    expect(Anagram.get("")).toBe("");
   });
 
   test("Should return 'b' if the input string is b", () => {
-    expect(Anagram.handler("b")).toBe("b");
+    expect(Anagram.get("b")).toBe("b");
   });
 
   test("Should return all anagrams of ab", () => {
-    expect(Anagram.handler("ab")).toBe("ab ba");
+    expect(Anagram.get("ab")).toBe("ab ba");
   });
 
   test("Should return all anagrams of abc", () => {
-    expect(Anagram.handler("abc")).toBe("abc acb bac bca cab cba");
+    expect(Anagram.get("abc")).toBe("abc acb bac bca cab cba");
   });
 
   test("Should return all anagrams of abcd", () => {
-    expect(Anagram.handler("abcd")).toBe(
+    expect(Anagram.get("abcd")).toBe(
       "abcd abdc acbd acdb adbc adcb bacd badc bcad bcda bdac bdca cabd cadb cbad cbda cdab cdba dabc dacb dbac dbca dcab dcba"
     );
   });
 
   test("Should return all anagrams of biro", () => {
-    expect(Anagram.handler("biro")).toBe(
+    expect(Anagram.get("biro")).toBe(
       "biro bior brio broi boir bori ibro ibor irbo irob iobr iorb rbio rboi ribo riob robi roib obir obri oibr oirb orbi orib"
     );
   });
